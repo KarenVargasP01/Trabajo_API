84bd59299b3ef363e0587d829b56f8978e8363fa
davparrado <davparrado7@gmail.com>
2026-02-13 21:25:06 -0500

    v3

--- Frontend/app.js
+++ Frontend/app.js
@@ -1,21 +1,20 @@
 
 
-
 let bt=document.getElementById("bt")
 let dato=document.getElementById("opciones")
 let url ="http://127.0.0.1:8000/getjson" // conexion de la url de la futura api
 
-let myAPI = url + "?localidad=" + dato.value;
 
 bt.addEventListener("click", () => {
-    console.log(myAPI)
+    let myAPI = url + "?localidad=" + dato.value;    
+    peticion(myAPI);
     
  }
 )
 
 // conexion remota se hace con fetch
 
-async function peticion (){
+async function peticion (myAPI){
     let response = await fetch (myAPI);
     let datos = response .json()
 }
